d4f6bc12e6d31174ddfbf1091127c79981508e4d
Jörg Prante <joergprante@gmail.com>
2014-02-01 00:28:22 +0100

    1.0.0.RC1.4

--- src/main/java/org/xbib/elasticsearch/river/jdbc/JDBCRiver.java
+++ src/main/java/org/xbib/elasticsearch/river/jdbc/JDBCRiver.java
@@ -1,19 +1,14 @@
 
 package org.xbib.elasticsearch.river.jdbc;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import org.elasticsearch.client.Client;
-import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
-import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.common.xcontent.XContentParser;
-import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.common.xcontent.support.XContentMapValues;
 import org.elasticsearch.river.AbstractRiverComponent;
 import org.elasticsearch.river.River;
@@ -91,9 +86,9 @@ public class JDBCRiver extends AbstractRiverComponent implements River {
         String locale = XContentMapValues.nodeStringValue(mySettings.get("locale"), LocaleUtil.fromLocale(Locale.getDefault()));
 
         // defaults for column strategy
-        String columnCreatedAt = XContentMapValues.nodeStringValue(mySettings.get("column_created_at"), "created_at");
-        String columnUpdatedAt = XContentMapValues.nodeStringValue(mySettings.get("column_updated_at"), "updated_at");
-        String columnDeletedAt = XContentMapValues.nodeStringValue(mySettings.get("column_deleted_at"), null);
+        String columnCreatedAt = XContentMapValues.nodeStringValue(mySettings.get("created_at"), "created_at");
+        String columnUpdatedAt = XContentMapValues.nodeStringValue(mySettings.get("updated_at"), "updated_at");
+        String columnDeletedAt = XContentMapValues.nodeStringValue(mySettings.get("deleted_at"), null);
         boolean columnEscape = XContentMapValues.nodeBooleanValue(mySettings.get("column_escape"), true);
 
         // set up bulk indexer. If no _index or _type pseudo columns are set in SQL statements, this index is used.
