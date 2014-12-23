3d90fb8e3b9e17bfeaf42f975f746d8a24b11efe
Jörg Prante <joergprante@gmail.com>
2014-12-23 09:37:56 +0100

    another cron expression test

--- src/test/java/org/xbib/cron/CronTest.java
+++ src/test/java/org/xbib/cron/CronTest.java
@@ -88,7 +88,7 @@ public class CronTest {
 
         expression = new CronExpression("0/2 * * * * ?");
         assertEquals(expression.getExpressionSummary(),
-                "seconds: 0\n" +
+                "seconds: 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58\n" +
                         "minutes: *\n" +
                         "hours: *\n" +
                         "daysOfMonth: *\n" +
