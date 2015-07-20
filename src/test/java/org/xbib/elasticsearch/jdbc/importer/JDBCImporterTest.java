5376216f71623522c614fa4aa228b93f7cc4e93f
Jörg Prante <joergprante@gmail.com>
2015-07-20 13:29:21 +0200

    JDBC importer test

---
+++ src/test/java/org/xbib/elasticsearch/jdbc/importer/JDBCImporterTest.java
@@ -0,0 +1,25 @@
+package org.xbib.elasticsearch.jdbc.importer;
+
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.testng.annotations.Test;
+import org.xbib.tools.JDBCImporter;
+
+public class JDBCImporterTest {
+
+    @Test
+    public void testImporter() throws Exception {
+        final JDBCImporter importer = JDBCImporter.getInstance();
+        Settings settings = ImmutableSettings.settingsBuilder()
+                .put("url", "jdbc:mysql://localhost:3306/test")
+                .put("password", "")
+                .put("sql", "select * from test")
+                .put("index", "jdbc")
+                .put("type", "jdbc")
+                .build();
+        importer.setSettings(settings);
+        importer.run();
+        Thread.sleep(12000L);
+        importer.shutdown();
+    }
+}
