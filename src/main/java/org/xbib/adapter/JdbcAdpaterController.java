b772fb2c82683070539726e261cd95d2a13cd9a5
neals <shandeai520@gmail.com>
2017-08-20 20:54:33 +0800

    add importer list

--- src/main/java/org/xbib/adapter/JdbcAdpaterController.java
+++ src/main/java/org/xbib/adapter/JdbcAdpaterController.java
@@ -1,8 +1,16 @@
 package org.xbib.adapter;
 
+import com.google.gson.Gson;
+import org.elasticsearch.common.settings.Settings;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.xbib.adapter.config.ConfigInfo;
+import org.xbib.adapter.config.ConfigedItem;
+import org.xbib.tools.JDBCImporter;
+
+import java.util.*;
+
+import static org.elasticsearch.common.settings.Settings.settingsBuilder;
 
 /**
  * Created by sanyu on 2017/8/20.
@@ -11,20 +19,51 @@ import org.xbib.adapter.config.ConfigInfo;
 @RequestMapping("api")
 public class JdbcAdpaterController {
 
-    @GetMapping("run")
-    public ResponseEntity<String> run(){
+    Map<String, JDBCImporter> map = new HashMap<>();
 
-        return ResponseEntity.ok("running...");
+    @GetMapping("run/{id}")
+    public ResponseEntity<String> run(@PathVariable String id){
+        JDBCImporter jdbcImporter = map.get(id);
+        jdbcImporter.run();
+        return ResponseEntity.ok("running..." + id);
     }
 
-    @GetMapping("shutdown")
-    public ResponseEntity<String> shutdown(){
-        return ResponseEntity.ok("shutdown...");
+    @GetMapping("shutdown/{id}")
+    public ResponseEntity<String> shutdown(@PathVariable String id){
+        JDBCImporter jdbcImporter = map.get(id);
+        jdbcImporter.run();
+        return ResponseEntity.ok("shutdown..." + id);
     }
 
     @PostMapping("config")
     public ResponseEntity<ConfigInfo> saveConfig(@RequestBody ConfigInfo configInfo){
+
+        Gson gson = new Gson();
+        String json = gson.toJson(configInfo);
+        Settings settings = settingsBuilder().loadFromSource(json).build();
+
+        JDBCImporter jdbcImporter = new JDBCImporter();
+        jdbcImporter.setSettings(settings);
+
+        map.put("importer" + jdbcImporter.hashCode(), jdbcImporter);
         return ResponseEntity.ok(configInfo);
+    }
+
+    @GetMapping("list")
+    public ResponseEntity<List<ConfigedItem>> getImporters(){
+        List<ConfigedItem> configedItems = new ArrayList<>();
+
+        Iterator iterator = map.keySet().iterator();
+        while(iterator.hasNext()){
+            String key = (String) iterator.next();
+
+            ConfigedItem configedItem = new ConfigedItem();
+            configedItem.setName(key);
+            configedItem.setIsActive(map.get(key).isActive());
+            configedItems.add(configedItem);
+        }
+
+        return ResponseEntity.ok(configedItems);
     }
 
 }
